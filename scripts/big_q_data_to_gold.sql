a040690596e7b6c5ce2af0a326c09ef002fab97f
alexptrw <115061948+alexptrw@users.noreply.github.com>
2025-06-29 13:26:31 +0000

    Gold layer script

--- scripts/big_q_data_to_gold.sql
+++ scripts/big_q_data_to_gold.sql
@@ -1,35 +1,47 @@
+/*
+Purpose:
+This script processes the silver data from the FMP dataset to create gold tables.
+
+============================================
+DDL script to create gold tables from the silver data in BigQuery.
+============================================
+Usage:
+Run this script in the BigQuery console.
+*/
+
+
 -- only forex exchange names
-CREATE OR REPLACE TABLE fmp_gold_data.forex_excange AS
+CREATE OR REPLACE TABLE fmp_data.forex_excange_gold AS
 SELECT symbol,
 currency,
 date_added,
-exchange
- FROM `terraform-demo-458815.fmp_silver_data.fmp_silver`
+exchange_code
+ FROM `stock-marketing-project.fmp_data.fmp_data_silver`
 WHERE is_forex is true;
 
 -- not forex exchange names
-CREATE OR REPLACE TABLE fmp_gold_data.forex_excange AS
+CREATE OR REPLACE TABLE fmp_data.non_forex_excange_gold AS
 SELECT symbol,
 currency,
 date_added,
-exchange
- FROM `terraform-demo-458815.fmp_silver_data.fmp_silver`
+exchange_code
+ FROM `stock-marketing-project.fmp_data.fmp_data_silver`
 WHERE is_forex is false;
 
 -- overview of unique exchanges + overall count of exchanges
-CREATE OR REPLACE TABLE fmp_gold_data.exchange_summary AS
-SELECT exchange, COUNT(DISTINCT symbol) as total_symbols, MAX(date_added) as latest_date_added
- FROM `terraform-demo-458815.fmp_silver_data.fmp_silver`
+CREATE OR REPLACE TABLE fmp_data.exchange_summary_gold AS
+SELECT exchange_code, COUNT(DISTINCT symbol) as total_symbols, MAX(date_added) as latest_date_added
+ FROM `stock-marketing-project.fmp_data.fmp_data_silver`
  GROUP BY exchange
  ORDER BY COUNT(DISTINCT symbol) desc;
 
--- compnies with multiple exchanges
-CREATE OR REPLACE TABLE fmp_gold_data.company_diff_exchange AS
+-- companies with multiple exchanges
+CREATE OR REPLACE TABLE fmp_data.company_diff_exchange_gold AS
 SELECT 
   company_name,
   COUNT(DISTINCT exchange) AS exchange_count,
   STRING_AGG(DISTINCT exchange, ', ') AS exchanges
-FROM `terraform-demo-458815.fmp_silver_data.fmp_silver`
+FROM `stock-marketing-project.fmp_data.fmp_data_silver`
 GROUP BY company_name
 HAVING exchange_count > 1
 ORDER BY company_name;
